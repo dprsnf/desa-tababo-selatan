b47fe6b492b5e786ac897a16b2976eced6f1927b
Mario Fernando Bernardino Paath <mariopaath23@gmail.com>
2025-12-07 17:45:36 +0800

    fix so paling fix

--- app/admin/pertanggungjawaban/page.tsx
+++ app/admin/pertanggungjawaban/page.tsx
@@ -284,7 +284,7 @@ export default function PertanggungjawabanPage() {
                         setPage((p) => Math.min(totalPages, p + 1))
                       }
                       disabled={page === totalPages}
-                      className="px-4 py-2 border border-gray-300 rounded-lg bg-white text-sm font-medium hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed text-gray-900">
+                      className="px-4 py-2 border border-gray-300 rounded-lg bg-white text-sm font-medium hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed text-gray-900"
                     >
                       Selanjutnya
                     </button>
